1386f3481da639324478f1c16d2d3f5ebc7e7fe9
Icaro Guedes <134503766+icaroguedes@users.noreply.github.com>
2023-09-20 07:48:24 -0300

    Add files via upload

---
+++ login/login.js
@@ -0,0 +1,13 @@
+lgn = "biro";
+pwd = "123456";
+
+function autenticar(){
+    var log = document.getElementById("login").value;
+    var pass = document.getElementById("pwd").value;
+
+    if(log == lgn & pass == pwd){
+        alert('Fon')
+    }else{
+        alert('Firifonfon')
+    }
+}
